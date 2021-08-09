1888c984cad230da9af83ff9eba65626bc73a6f4
Ronald Main <rbmain84@yahoo.com>
2021-08-09 00:42:28 -0500

    nav

--- src/App.js
+++ src/App.js
@@ -1,39 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
-import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
+import Nav from './components/nav';
 import Main from './components/main';
-import { Link } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-class App extends Component {
-  render () {
+function App() {
     return (
-      <div className="demo-big-content">
-    <Layout>
-        <Header className="header-color" title="Title" scroll>
-            <Navigation>
-                <Link to="/resume">Resume</Link>
-                <Link to="/aboutme">About Me</Link>
-                <Link to="/projects">Projects</Link>
-                <Link to="/contact">Contact</Link>
-            </Navigation>
-        </Header>
-        <Drawer title="Title">
-            <Navigation>
-            <Link to="/resume">Resume</Link>
-                <Link to="/aboutme">About Me</Link>
-                <Link to="/projects">Projects</Link>
-                <Link to="/contact">Contact</Link>
-            </Navigation>
-        </Drawer>
-        <Content>
-            <div className="page-content" />
-            <Main/>
-        </Content>
-    </Layout>
-</div>
+      <div>
+          <Nav></Nav>
+          <Main></Main>
+      </div>
     );
-    
   }
-}
 
 export default App;
